a3a66d8a8257163f2f52c1e704c0b5c9121df339
Thomas Hunter <thomas.hunter@alfresco.com>
2021-03-30 09:28:45 +0100

    [AAE-4428] Add selected file counter to attach file widget

--- lib/content-services/src/lib/content-node-selector/content-node-selector-panel.component.ts
+++ lib/content-services/src/lib/content-node-selector/content-node-selector-panel.component.ts
@@ -38,7 +38,8 @@ import {
     FileUploadDeleteEvent,
     FileModel,
     AppConfigService,
-    DataSorting
+    DataSorting,
+    TranslationService
 } from '@alfresco/adf-core';
 import { FormControl } from '@angular/forms';
 import { Node, NodePaging, Pagination, SiteEntry, SitePaging, NodeEntry, QueryBody, RequestScope } from '@alfresco/js-api';
@@ -266,7 +267,8 @@ export class ContentNodeSelectorPanelComponent implements OnInit, OnDestroy {
                 private uploadService: UploadService,
                 private sitesService: SitesService,
                 private appConfigService: AppConfigService,
-                private contentNodeSelectorPanelService: ContentNodeSelectorPanelService) {
+                private contentNodeSelectorPanelService: ContentNodeSelectorPanelService,
+                private translation: TranslationService) {
     }
 
     set chosenNode(value: Node[]) {
@@ -276,6 +278,10 @@ export class ContentNodeSelectorPanelComponent implements OnInit, OnDestroy {
 
     get chosenNode() {
         return this._chosenNode;
+    }
+
+    getSelectedCountTranslation(): string {
+        return this.translation.instant(`NODE_SELECTOR.SELECTED_COUNT`, { count: this.chosenNode?.length || 0 });
     }
 
     ngOnInit() {
